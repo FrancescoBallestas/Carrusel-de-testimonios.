a06e0af08976a58084388524663c4ee84d5e08fe
Francesco Ballestas <franciballestas@gmail.com>
2023-10-11 12:22:04 -0500

    add:: efectos de transición

--- script.js
+++ script.js
@@ -1,16 +1,19 @@
 const container = document.querySelector('.container-dos');
 const testimonios = document.querySelectorAll('.testimonio');
-const tiempoEspera = 4000; // Tiempo en milisegundos (3 segundos en este ejemplo)
+const tiempoEspera = 3000;
 let posicionActual = 0;
 
 function moverCarrusel() {
     testimonios[posicionActual].style.display = 'none';
+
     posicionActual = (posicionActual + 1) % testimonios.length;
-    testimonios[posicionActual].style.display = 'block';
+
+    testimonios[posicionActual].style.display = 'inline-block';
 }
 
 function iniciarCarrusel() {
-    testimonios[posicionActual].style.display = 'block';
+    testimonios[0].style.display = 'inline-block'; // Mostrar el primer testimonio inicialmente
+
     setInterval(moverCarrusel, tiempoEspera);
 }
 
